ed60023ea11f59088c9b6dbbad8e677f47ddccc0
Logan <loganmsorensen@gmail.com>
2021-06-29 11:40:10 -0700

    setup less structure, built sidebar and chatWindow components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Sidebar from "./components/sidebar";
+import ChatWindow from "./components/chatWindow";
 
 function App() {
   return (
     <div className="App">
-    
+      <Sidebar />
+      <ChatWindow />
     </div>
   );
 }
---
+++ src/components/chatWindow.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatWindow = () => {
+  return (
+    <div className="chat-window">
+      <div className="channel-name-tab">Placeholder Name</div>
+    </div>
+  );
+};
+
+export default ChatWindow;
