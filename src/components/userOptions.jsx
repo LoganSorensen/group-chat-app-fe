79818fba213ad2338aebe26591cb2e0d5ec60467
Logan <loganmsorensen@gmail.com>
2021-07-01 17:08:08 -0700

    built and styled userOptions component

---
+++ src/components/userOptions.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+
+const UserOptions = ({ optionsOpen, setOptionsOpen }) => {
+  const history = useHistory();
+  const logout = () => {
+    localStorage.removeItem("token");
+    history.push("/login");
+  };
+
+  const handleBlur = (e) => {
+    const optionsBtn = document.getElementById("options-btn");
+    console.log("running");
+    if (
+      !e.currentTarget.contains(e.relatedTarget) &&
+      e.relatedTarget !== optionsBtn
+    )
+      setOptionsOpen(!optionsOpen);
+  };
+
+  return (
+    <div
+      className="user-options"
+      id="user-options"
+      style={{ display: optionsOpen ? "block" : "none" }}
+      tabIndex="0"
+      onBlur={handleBlur}
+    >
+      <Link to="/profile" className="align-center">
+        <span className="material-icons">account_circle</span>
+        My Profile
+      </Link>
+      <Link to="#" className="align-center">
+        <span className="material-icons-outlined">landscape</span>
+        Tweeter
+      </Link>
+      <hr />
+      <button className="logout-btn align-center" onClick={logout}>
+        <span className="material-icons-outlined">exit_to_app</span>Logout
+      </button>
+    </div>
+  );
+};
+
+export default UserOptions;
--- src/components/userTab.jsx
+++ src/components/userTab.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { members } from "../utils/data";
+import UserOptions from "./userOptions";
 
 const UserTab = () => {
+  const [optionsOpen, setOptionsOpen] = useState(false);
+
+  const toggleUserOptions = () => setOptionsOpen(!optionsOpen);
+
   return (
     <div className="user-tab align-center justify-between">
       <div className="user-info align-center">
@@ -11,9 +16,16 @@ const UserTab = () => {
         </div>
         <p className="user-name">{members[0].name}</p>
       </div>
-      <button className="options-btn align-center">
-        <span className="material-icons-outlined">expand_more</span>
+      <button className="options-btn align-center" id="options-btn">
+        <span
+          className="material-icons-outlined"
+          style={{ transform: optionsOpen ? "rotate(180deg)" : null }}
+          onClick={toggleUserOptions}
+        >
+          expand_more
+        </span>
       </button>
+      <UserOptions optionsOpen={optionsOpen} setOptionsOpen={setOptionsOpen} />
     </div>
   );
 };
